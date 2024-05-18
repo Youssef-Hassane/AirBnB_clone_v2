782082dfc2dc27a33b1322907430f547f61e3611
youssef-hassane <youssef_hassane@icloud.com>
2024-05-18 16:52:27 +0300

    Task N-3 - DONE

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,101 +1,79 @@
 #!/usr/bin/python3
+""" Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers """
 
-"""
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers.
-
-Usage: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
-"""
-
-from fabric.api import env, local, put, local
-from datetime import datetime
+import time
+from fabric.api import local, put, env, run
 from os.path import exists, isdir
-
-
-# Define the host servers
-env.hosts = ['54.173.82.140', '54.157.177.171']
+env.hosts = ['54.160.86.192', '54.160.113.163']
 
 
 def do_pack():
-    """
-    Creates an archive from the contents of the web_static folder.
-    Returns the archive path if successful, otherwise returns None.
-    """
+    """ A function that generates a .tgz archive """
     try:
-        # Create an archive from the web_static folder
         file_name = "versions/web_static_{}.tgz".format(
-            datetime.strftime("%Y%m%d%H%M%S"))
-        if not isdir("versions"):
-            # Create the versions directory if it doesn't exist
+            time.strftime("%Y%m%d%H%M%S"))
+        if isdir("versions") is False:
             local("mkdir versions")
-        # Create the tgz archive
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-
     except Exception:
         return None
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to the web servers.
-
-    Parameters:
-    archive_path (str): The path to the archive file to be distributed.
-
-    Returns:
-    bool: True if the deployment was successful, False otherwise.
-    """
-    if not exists(archive_path):
+    """ A function that distributes an archive to your web servers """
+    try:
+        if exists(archive_path):
+            file_name = archive_path.split("/")[-1]
+            file_no_ext = file_name.split(".")[0]
+            path = "/data/web_static/releases/"
+            put(archive_path, "/tmp/")
+            run('mkdir -p {}{}/'.format(path, file_no_ext))
+            run('tar -xzf /tmp/{} -C {}{}/'.format(
+                file_name, path, file_no_ext))
+            run('rm /tmp/{}'.format(file_name))
+            run('mv {0}{1}/web_static/* {0}{1}/'.format(
+                path, file_no_ext))
+            run('rm -rf {}{}/web_static'.format(path, file_no_ext))
+            run('rm -rf /data/web_static/current')
+            run('ln -fs {}{}/ /data/web_static/current'.format(
+                path, file_no_ext))
+            return True
+        else:
+            return False
+    except Exception:
         return False
 
+
+def do_deploy_local(archive_path):
+    """ A function that distributes an archive to your local machine """
     try:
-        # Extract the archive file name and its base name without extension
-        archive_file = archive_path.split("/")[-1]
-        base_name = archive_file.split(".")[0]
-        release_path = "/data/web_static/releases/"
-
-        # Upload the archive to the /tmp/ directory on the web server
-        local("cp {} /tmp/".format(archive_path))
-
-        # Create the release directory on the web server
-        local('mkdir -p {}{}'.format(release_path, base_name))
-
-        # Uncompress the archive to the release directory
-        local('tar -xzf /tmp/{} -C {}{}'.format(archive_file,
-                                              release_path, base_name))
-
-        # Remove the uploaded archive from /tmp/
-        local('rm /tmp/{}'.format(archive_file))
-
-        # Move the contents out of the web_static subdirectory
-        local('mv {0}{1}/web_static/* {0}{1}/'.format(release_path, base_name))
-
-        # Remove the now-empty web_static directory
-        local('rm -rf {}{}/web_static'.format(release_path, base_name))
-
-        # Remove the current symbolic link
-        local('rm -rf /data/web_static/current')
-
-        # Create a new symbolic link to the new release
-        local('ln -s {}{}/ /data/web_static/current'.format(release_path,
-                                                          base_name))
-
-        return True
+        if exists(archive_path):
+            file_name = archive_path.split("/")[-1]
+            file_no_ext = file_name.split(".")[0]
+            path = "/data/web_static/releases/"
+            local("cp {} /tmp/".format(archive_path))
+            local('mkdir -p {}{}/'.format(path, file_no_ext))
+            local('tar -xzf /tmp/{} -C {}{}/'.format(
+                file_name, path, file_no_ext))
+            local('rm /tmp/{}'.format(file_name))
+            local('mv {0}{1}/web_static/* {0}{1}/'.format(
+                path, file_no_ext))
+            local('rm -rf {}{}/web_static'.format(path, file_no_ext))
+            local('rm -rf /data/web_static/current')
+            local('ln -fs {}{}/ /data/web_static/current'.format(
+                path, file_no_ext))
+            return True
+        else:
+            return False
     except Exception:
         return False
 
 
 def deploy():
-    """
-    Creates and distributes an archive to the web servers.
-
-    Returns:
-    bool: True if both the creation and deployment were successful,
-    False otherwise.
-    """
-    # archive_path = do_pack()
-    # if archive_path is None:
-    #     return False
-    # return do_deploy(archive_path)
-    return do_deploy(do_pack())
+    """ A function that distributes an archive to your web servers """
+    # if env.hosts == ['54.160.86.192', '54.160.113.163']:
+    #     return do_deploy(do_pack())
+    # else:
+    return do_deploy_local(do_pack())
