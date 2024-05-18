1126a0c5b7fbeaf165293568a32223173cb5302d
youssef-hassane <youssef_hassane@icloud.com>
2024-05-18 16:24:37 +0300

    Task N-3 - DONE

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers.
 Usage: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 """
 
-from fabric.api import env, local, put, run
+from fabric.api import env, local, put, local
 from datetime import datetime
 from os.path import exists, isdir
 
@@ -59,63 +59,14 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
 
         # Create the release directory on the web server
-        run('mkdir -p {}{}'.format(release_path, base_name))
+        local('mkdir -p {}{}'.format(release_path, base_name))
 
         # Uncompress the archive to the release directory
-        run('tar -xzf /tmp/{} -C {}{}'.format(archive_file,
+        local('tar -xzf /tmp/{} -C {}{}'.format(archive_file,
                                               release_path, base_name))
 
         # Remove the uploaded archive from /tmp/
-        run('rm /tmp/{}'.format(archive_file))
-
-        # Move the contents out of the web_static subdirectory
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(release_path, base_name))
-
-        # Remove the now-empty web_static directory
-        run('rm -rf {}{}/web_static'.format(release_path, base_name))
-
-        # Remove the current symbolic link
-        run('rm -rf /data/web_static/current')
-
-        # Create a new symbolic link to the new release
-        run('ln -s {}{}/ /data/web_static/current'.format(release_path,
-                                                          base_name))
-
-        return True
-    except Exception:
-        return False
-
-
-def do_deploy_local(archive_path):
-    """
-    Distributes an archive to your local machine.
-
-    Parameters:
-    archive_path (str): The path to the archive file to be distributed.
-
-    Returns:
-    bool: True if the deployment was successful, False otherwise.
-    """
-    try:
-        if not exists(archive_path):
-            return False
-
-        archive_file = archive_path.split("/")[-1]
-        base_name = archive_file.split(".")[0]
-        release_path = "/data/web_static/releases/"
-
-        # Copy the archive to the /tmp/ directory
-        local("cp {} /tmp/".format(archive_path))
-
-        # Create the release directory
-        local("mkdir -p {}{}".format(release_path, base_name))
-
-        # Uncompress the archive to the release directory
-        local("tar -xzf /tmp/{} -C {}{}".format(archive_file, release_path,
-                                                base_name))
-
-        # Remove the copied archive from /tmp/
-        local("rm /tmp/{}".format(archive_file))
+        local('rm /tmp/{}'.format(archive_file))
 
         # Move the contents out of the web_static subdirectory
         local('mv {0}{1}/web_static/* {0}{1}/'.format(release_path, base_name))
@@ -127,11 +78,11 @@ def do_deploy_local(archive_path):
         local('rm -rf /data/web_static/current')
 
         # Create a new symbolic link to the new release
-        local('ln -fs {}{}/ /data/web_static/current'.format(release_path,
-                                                             base_name))
+        local('ln -s {}{}/ /data/web_static/current'.format(release_path,
+                                                          base_name))
 
         return True
-    except Exception as e:
+    except Exception:
         return False
 
 
@@ -143,8 +94,7 @@ def deploy():
     bool: True if both the creation and deployment were successful,
     False otherwise.
     """
-    # archive_path = do_pack()
-    # if archive_path is None:
-    #     return False
-    # return do_deploy(archive_path)
-    return do_deploy_local(do_pack())
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
