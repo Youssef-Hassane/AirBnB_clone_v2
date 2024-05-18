f789501b2f0d35495582c8d84c12d607bd6483fa
youssef-hassane <youssef_hassane@icloud.com>
2024-05-18 13:44:25 +0300

    Fixing Errors

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,8 +3,7 @@
 import os
 from fabric.api import *
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
-
+env.hosts = ['54.173.82.140', '	54.157.177.171']
 
 def do_clean(number=0):
     """Delete out-of-date archives.
