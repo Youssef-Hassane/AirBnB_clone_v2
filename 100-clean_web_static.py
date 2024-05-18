545123e55a2ae3b68e2a17dbf5e0ba58aeb292b0
youssef-hassane <youssef_hassane@icloud.com>
2024-05-18 09:15:14 +0300

    Fixing Errors

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -4,15 +4,9 @@ from fabric.api import *
 
 env.hosts = ['54.173.82.140', '54.157.177.171']
 
+
 def do_clean(number=0):
     """Delete out-of-date archives.
-
-    Args:
-        number (int): The number of archives to keep.
-
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
     """
     number = 1 if int(number) == 0 else int(number)
 
