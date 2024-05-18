08463c9adaefacd9c2de0f09bae4e1c07dbd4b29
youssef-hassane <youssef_hassane@icloud.com>
2024-05-18 08:59:42 +0300

    Fixing Errors

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,37 +1,28 @@
 #!/usr/bin/python3
 """
-Deletes out-of-date archives.
-Usage: fab -f 100-clean_web_static.py do_clean:number=2
-    -i ssh-key -u ubuntu > /dev/null 2>&1
+Fabric script that deletes out-of-date archives using the function do_clean
 """
 
-import os
 from fabric.api import *
+import os
 
+env.user = 'ubuntu'
 env.hosts = ['54.173.82.140', '54.157.177.171']
+env.key_filename = ['~/.ssh/id_rsa']
+
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
-    
-    Args:
-        number (int): The number of archives to keep.
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
-    """
-    number = 1 if int(number) == 0 else int(number)
+    """Deletes out-of-date archives"""
+    try:
+        number = int(number)
+        if number < 1:
+            number = 1
 
-    # Local cleanup
-    archives = sorted(os.listdir("versions"))
-    archives_to_delete = archives[:-number]
-    with lcd("versions"):
-        for archive in archives_to_delete:
-            local("rm ./{}".format(archive))
+        with lcd("versions"):
+            local("ls -t | tail -n +{} | xargs rm -rf".format(number + 1))
 
-    # Remote cleanup
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        archives_to_delete = archives[:-number]
-        for archive in archives_to_delete:
-            run("rm -rf ./{}".format(archive))
+        with cd("/data/web_static/releases"):
+            run("ls -t | tail -n +{} | xargs rm -rf".format(number + 1))
+    except Exception as e:
+        print(e)
+        return False
