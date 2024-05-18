bc0340393545bfe5808db46f619187ce375f5f0a
youssef-hassane <youssef_hassane@icloud.com>
2024-05-18 15:41:19 +0300

    Task #1 - DONE

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,8 +14,8 @@ def do_pack():
     """
 
     current_time = datetime.now()
-    archive_name = \
-    'web_static_' + current_time.strftime("%Y%m%d%H%M%S") + '.tgz'
+    archive_name = 'web_static_' + \
+        current_time.strftime("%Y%m%d%H%M%S") + '.tgz'
 
     # Create the versions directory if it doesn't exist
     local('mkdir -p versions')
@@ -24,7 +24,7 @@ def do_pack():
     result = local('tar -cvzf versions/{} web_static'.format(archive_name))
 
     # Check if the archive was created successfully
-    if result is not None:
+    if result.succeeded:
         return archive_name
     else:
         return None
