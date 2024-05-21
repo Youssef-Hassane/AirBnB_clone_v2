5b05970120e7feaf026e04a7c4ba6853c982715a
youssef-hassane <youssef_hassane@icloud.com>
2024-05-21 18:19:18 +0300

    Task 6 - DONE

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -48,6 +48,7 @@ def number_template(n):
     """Return an HTML page at the /number_template/<n> URL."""
     return render_template('5-number.html', n=n)
 
+
 # Define a route for the /number_odd_or_even/<n> URL and disable strict slashes
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
