e13fca4fd951c9be453bd7139b7d02728d27fcac
youssef-hassane <youssef_hassane@icloud.com>
2024-05-21 11:09:57 +0300

    Task 0 - DONE

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# Define a route for the root URL ("/") and disable strict slashes
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a string at the root URL."""
+    return 'Hello HBNB!'
+
+
+# Run the Flask application if this file is executed directly
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
