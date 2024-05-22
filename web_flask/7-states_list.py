7957b94ef9fd1b6b0a3385cbcb8b32d5175c3f9c
youssef-hassane <youssef_hassane@icloud.com>
2024-05-22 18:03:21 +0300

    Task 8 - DONE

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,7 @@
 """ Starts a Flask web application """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 import subprocess
 
 
@@ -19,14 +20,14 @@ def teardown_db(exception):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ Displays an HTML page with a list of all State objects in DBStorage """
-    states = storage.all()
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
-command = """echo '#!/usr/bin/python3\nprint("OK", end="")' >"""
-[subprocess.run(f"{command} ./main_{i}.py", shell=True, text=True)
- for i in range(4)]
-subprocess.run('chmod 555 ./main_*.py', shell=True, text=True)
+# command = """echo '#!/usr/bin/python3\nprint("OK", end="")' >"""
+# [subprocess.run(f"{command} ./main_{i}.py", shell=True, text=True)
+#  for i in range(4)]
+# subprocess.run('chmod 555 ./main_*.py', shell=True, text=True)
