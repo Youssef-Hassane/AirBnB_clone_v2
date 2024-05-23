7d5c877eaa2cef53db53df9e579fa80ae19c6288
youssef-hassane <youssef_hassane@icloud.com>
2024-05-23 04:04:54 +0300

    Task 12 - DONE

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template
 from models import storage
 from models.amenity import Amenity
 from models.state import State
+from models.place import Place
+
 
 # Create a new Flask web application
 app = Flask(__name__)
@@ -23,8 +25,9 @@ def display_hbnb_filters():
     # Get all states and amenities from the storage
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
+    places = storage.all(Place).values()
     # Render the template '10-hbnb_filters.html' with the states and amenities
-    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+    return render_template('100-hbnb.html', states=states, amenities=amenities, places=places)
 
 
 if __name__ == "__main__":
